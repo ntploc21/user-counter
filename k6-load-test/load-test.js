728d7d7bad2bab541a835407cb7cf36deab10ee3
locntp <locntp@vng.com.vn>
2025-10-10 17:30:21 +0700

    update: update README.md to add techstack, installation, and software design

--- k6-load-test/load-test.js
+++ k6-load-test/load-test.js
@@ -2,14 +2,17 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-    vus: 10, // number of virtual users
-    duration: '30s', // test duration
+    stages: [
+        { duration: '30s', target: 200 }, // Ramp-up to 200 users over 30 seconds
+        { duration: '1m', target: 200 },  // Stay at 200 users for 1 minute
+        { duration: '10s', target: 0 },   // Ramp-down to 0 users over 10 seconds
+    ],
 };
 
 export default function () {
     // 1. Create user counter
     let username = 'user' + Math.floor(Math.random() * 1000000);
-    let createRes = http.post('http://localhost:63061/api/v1/users', JSON.stringify({ username: username }), {
+    let createRes = http.post('http://locntp-user.zalopay.vn/api/v1/users', JSON.stringify({ username: username }), {
         headers: {
             'Content-Type': 'application/json',
         },
@@ -18,12 +21,16 @@ export default function () {
     let userId = createRes.json().data.id;
 
     // 2. Increase user counter
-    let incRes = http.put(`http://localhost:63061/api/v1/users/${userId}/increment`);
+    let incRes = http.put(`http://locntp-user.zalopay.vn/api/v1/users/${userId}/increment`);
     check(incRes, { 'increase user counter status is 200': (r) => r.status === 200 });
 
     // 3. Get user counter
-    let getRes = http.get(`http://localhost:63061/api/v1/users/${userId}/count`);
+    let getRes = http.get(`http://locntp-user.zalopay.vn/api/v1/users/${userId}/count`);
     check(getRes, { 'get user counter status is 200': (r) => r.status === 200 });
+
+    // 4. Delete user counter
+    let delRes = http.del(`http://locntp-user.zalopay.vn/api/v1/users/${userId}`);
+    check(delRes, { 'delete user counter status is 200': (r) => r.status === 200 });
 
     sleep(1);
 }
